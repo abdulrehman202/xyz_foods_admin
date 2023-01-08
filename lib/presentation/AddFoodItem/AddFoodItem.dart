c067ff28fee838ac7b5fe1301975f243412db67b
abdulrehman202 <abdulrehman202@users.noreply.github.com>
2023-01-08 20:37:57 +0500

    addfooditem ui updated

--- lib/presentation/AddFoodItem/AddFoodItem.dart
+++ lib/presentation/AddFoodItem/AddFoodItem.dart
@@ -1,5 +1,10 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:xyz_foods_admin/presentation/Components/InputTextFields.dart';
+import 'package:xyz_foods_admin/presentation/resources/assets_manager.dart';
+import 'package:xyz_foods_admin/presentation/resources/color_manager.dart';
+import 'package:xyz_foods_admin/presentation/resources/values_manager.dart';
 
 class AddFoodItem extends StatefulWidget {
   const AddFoodItem({super.key});
@@ -9,8 +14,60 @@ class AddFoodItem extends StatefulWidget {
 }
 
 class _AddFoodItemState extends State<AddFoodItem> {
+  late InputTextField inputCategory, inputName, inputPrice;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    inputCategory = InputTextField(fieldName: 'Category');
+    inputName = InputTextField(fieldName: 'Name');
+    inputPrice = InputTextField(fieldName: 'Price');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        backgroundColor: ColorManager.primaryOpacity70,
+        body: Row(children: [
+          Container(
+            width: MediaQuery.of(context).size.width * 0.5,
+            child: Image.network(
+              'https://healthwire.pk/wp-content/uploads/2022/04/disadvantages-of-fast-food.jpg',
+              fit: BoxFit.fill,
+            ),
+          ),
+          Expanded(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                decoration: const BoxDecoration(
+                    borderRadius:
+                        BorderRadius.all(Radius.circular(AppSize.s20))),
+                padding: const EdgeInsets.all(AppPadding.p20),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.3,
+                      child: const Image(
+                        image: AssetImage(ImagePath.splashLogo),
+                      ),
+                    ),
+                    inputCategory,
+                    inputName,
+                    inputPrice,
+                    Container(
+                        alignment: Alignment.bottomLeft,
+                        margin: const EdgeInsets.all(AppMargin.m12),
+                        child: ElevatedButton(
+                            onPressed: () {}, child: const Text('Add dish')))
+                  ],
+                ),
+              ),
+            ],
+          ))
+        ]));
   }
 }
---
+++ lib/presentation/Components/InputTextFields.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:xyz_foods_admin/presentation/resources/values_manager.dart';
+
+import '../resources/color_manager.dart';
+
+class InputTextField extends StatefulWidget {
+  late String fieldName;
+  InputTextField({required this.fieldName, super.key});
+
+  @override
+  State<InputTextField> createState() => _InputTextFieldState();
+}
+
+class _InputTextFieldState extends State<InputTextField> {
+  var backgroundCursorColor;
+  var focusNode;
+  var style;
+  var controller;
+  var cursorColor;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    style = const TextStyle(decoration: TextDecoration.underline);
+    controller = TextEditingController();
+    focusNode = FocusNode();
+    backgroundCursorColor = ColorManager.primary;
+    cursorColor = ColorManager.primary;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Container(
+        padding: const EdgeInsets.all(AppPadding.p12),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SizedBox(width: AppSize.s60, child: Text(widget.fieldName)),
+            const VerticalDivider(),
+            Expanded(
+              child: EditableText(
+                controller: controller,
+                focusNode: focusNode,
+                style: style,
+                cursorColor: cursorColor,
+                backgroundCursorColor: backgroundCursorColor,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
